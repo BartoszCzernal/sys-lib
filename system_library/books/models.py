47485c0d7da2d3ae68c466eb878d99fdf6c5bb4e
BartoszCzernal <bartekczernal@gmail.com>
2021-01-06 12:06:11 +0100

    Add book model

--- system_library/books/models.py
+++ system_library/books/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13)
+    title = models.CharField(max_length=100)
+    date_of_release = models.DateField()
